ea80e9ed1e9e1d0e49b17da1fdb2d150aa997558
Roy <wiggerman.r@googlemail.com>
2022-02-11 14:04:22 +0100

    commit

--- Project/## Scripts ##/PRG-08/Ex 02 test 2.py
+++
@@ -1,3 +0,0 @@
-f = open("test_file.csv", W)
-f.w("test text")
-f.close
--- Project/## Scripts ##/PRG-08/Ex 02.py
+++ Project/## Scripts ##/PRG-08/Ex 02.py
@@ -1,17 +1,19 @@
 import csv
 
-keys = ["First name", "Last Name", "Job title", "Company"]
+keys = ["First name","Last Name","Job title","Company"]
 
-knv = {   "First name" : input("First name: "),
-          "Last name" : input("Last name: "),
-          "Job title" : input("Job title: "),
-          "Company" : input("Company: ") }
+knv = {   "First name " : input ("First name: "),
+          "Last name " : input ("Last name: "),
+          "Job title " : input ("Job title: "),
+          "Company " : input ("Company: ") }
 
 filename = "Roy_test.csv"
 
 with open(filename, "a") as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames = keys)
-    writer.writeheader()
-    writer.writerows(knv)
+    writer = csv.writer(csvfile)
+    writer.writerow(knv.keys())
+    writer.writerow(knv.values())
 
-print (knv)
+    print (knv)
+
+
